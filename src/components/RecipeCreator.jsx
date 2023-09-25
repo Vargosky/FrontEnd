cf175c2adfe4422eaa98ea9a8704dbbf50473e0e
Vargosky <sarem.vargas@gmail.com>
2023-09-25 20:46:38 -0300

    Update RecipeCreator.jsx

--- src/components/RecipeCreator.jsx
+++ src/components/RecipeCreator.jsx
@@ -70,7 +70,7 @@ const RecipeCreator = () => {
             // Aquí puedes agregar un mensaje para informar que se canceló la acción si lo deseas.
         }
     };
-    
+
 
     return (
         <div className="p-8 bg-orange-100 rounded-3xl shadow-2xl">
@@ -78,57 +78,57 @@ const RecipeCreator = () => {
             <div className="mb-4 text-center rounded-2xl">
                 <div className=' bg-orange-50 shadow-2xl p-10 text-center rounded-2xl my-10'>
                     <h3 className='text-center'> </h3>
-                <input
-                    type="text"
-                    placeholder="Nombre de la receta"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    className="p-2 border rounded"
-                />
-                <select
-                    value={category}
-                    onChange={(e) => setCategory(e.target.value)}
-                    className="p-2 border rounded w-2/3"
-                >
-                    <option value="">Selecciona el tipo de receta</option>
-                    <option value="Masa">Masa</option>
-                    <option value="Relleno">Relleno</option>
-                </select>
+                    <input
+                        type="text"
+                        placeholder="Nombre de la receta"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        className="p-2 border rounded"
+                    />
+                    <select
+                        value={category}
+                        onChange={(e) => setCategory(e.target.value)}
+                        className="p-2 border rounded w-2/3"
+                    >
+                        <option value="">Selecciona el tipo de receta</option>
+                        <option value="Masa">Masa</option>
+                        <option value="Relleno">Relleno</option>
+                    </select>
 
                 </div>
-            <h3 className='text-2xl font-bold mb-4 text-orange-500 text-center'>Ingredientes</h3> 
-            <div className='bg-orange-50 my-5 shadow-2xl p-10 rounded-2xl'>
-                <input
-                    type="number"
-                    placeholder="Cantidad"
-                    value={quantity}
-                    onChange={(e) => setQuantity(e.target.value)}
-                    className="p-2 border rounded"
-                />
+                <h3 className='text-2xl font-bold mb-4 text-orange-500 text-center'>Ingredientes</h3>
+                <div className='bg-orange-50 my-5 shadow-2xl p-10 rounded-2xl'>
+                    <input
+                        type="number"
+                        placeholder="Cantidad"
+                        value={quantity}
+                        onChange={(e) => setQuantity(e.target.value)}
+                        className="p-2 border rounded"
+                    />
 
 
-                <select
-                    value={selectedMaterial}
-                    onChange={(e) => setSelectedMaterial(e.target.value)}
-                    className="p-2 border rounded w-2/3"
-                >
-                    <option value="">Selecciona un material</option>
-                    {materials.map((material) => (
-                        <option key={material.id} value={material.nombre}>
-                            {material.nombre}
-                        </option>
-                    ))}
-                </select>
-            <div className='w-full text-center'>
-            <button
-                    onClick={handleAddMaterial}
-                    className={`p-2 rounded my-10 ${isEditing ? 'bg-green-500' : 'bg-blue-500'} text-white`}
-                >
-                    {isEditing ? 'Actualizar' : 'Agregar material'}
-                </button>   
-            </div>
-            
-            </div>
+                    <select
+                        value={selectedMaterial}
+                        onChange={(e) => setSelectedMaterial(e.target.value)}
+                        className="p-2 border rounded w-2/3"
+                    >
+                        <option value="">Selecciona un material</option>
+                        {materials.map((material) => (
+                            <option key={material.id} value={material.nombre}>
+                                {material.nombre}
+                            </option>
+                        ))}
+                    </select>
+                    <div className='w-full text-center'>
+                        <button
+                            onClick={handleAddMaterial}
+                            className={`p-2 rounded my-10 ${isEditing ? 'bg-green-500' : 'bg-blue-500'} text-white`}
+                        >
+                            {isEditing ? 'Actualizar' : 'Agregar material'}
+                        </button>
+                    </div>
+
+                </div>
 
 
                 {/* <button
@@ -139,46 +139,38 @@ const RecipeCreator = () => {
                 </button>    */}
 
             </div>
-{/* 
-            <div className="flex items-center space-x-4 mx-auto">
-            <button
-                    onClick={handleAddMaterial}
-                    className={`p-2 rounded ${isEditing ? 'bg-green-500' : 'bg-blue-500'} text-white`}
-                >
-                    {isEditing ? 'Actualizar' : '+'}
-                </button>   
-            </div> */}
 
-<div className="my-10">
-    <table className="min-w-full table-auto bg-orange-100 rounded-lg overflow-hidden">
-        <thead>
-            <tr className="text-orange-500 bg-orange-200">
-                <th className="px-4 py-2 text-left">Material</th>
-                <th className="px-4 py-2 text-left">Cantidad</th>
-                <th className="px-4 py-2">Editar</th>
-                <th className="px-4 py-2">Borrar</th>
-            </tr>
-        </thead>
-        <tbody>
-            {recipes.map((recipe, index) => (
-                <tr key={index} className="border-t border-orange-300">
-                    <td className="px-4 py-2">{recipe.material}</td>
-                    <td className="px-4 py-2">{recipe.quantity}</td>
-                    <td className="px-4 py-2 text-center">
-                        <button onClick={() => handleEditMaterial(index)} className="text-orange-500">
-                            <FiEdit2 />
-                        </button>
-                    </td>
-                    <td className="px-4 py-2 text-center">
-                        <button onClick={() => handleDeleteMaterial(index)} className="text-orange-500">
-                            <FiTrash2 />
-                        </button>
-                    </td>
-                </tr>
-            ))}
-        </tbody>
-    </table>
-</div>
+            <div className="my-10">
+                <table className="min-w-full table-auto bg-orange-100 rounded-lg overflow-hidden">
+                    <thead>
+                        <tr className="text-orange-500 bg-orange-200">
+                            <th className="px-4 py-2 text-left">Material</th>
+                            <th className="px-4 py-2 text-left">Cantidad</th>
+                            <th className="px-4 py-2">Editar</th>
+                            <th className="px-4 py-2">Borrar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {recipes.map((recipe, index) => (
+                            <tr key={index} className="border-t border-orange-300">
+                                <td className="px-4 py-2">{recipe.material}</td>
+                                <td className="px-4 py-2">{recipe.quantity}</td>
+                                <td className="px-4 py-2 text-center">
+                                    <button onClick={() => handleEditMaterial(index)} className="text-orange-500">
+                                        <FiEdit2 />
+                                    </button>
+                                </td>
+                                <td className="px-4 py-2 text-center">
+                                    <button onClick={() => handleDeleteMaterial(index)} className="text-orange-500">
+                                        <FiTrash2 />
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+
 
             <button
                 onClick={handleSave}
